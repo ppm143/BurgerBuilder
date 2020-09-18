8a69a228989c3135b678d36312c95d020b106363
ppm_143 <parthpatel.py@gmail.com>
2020-09-18 12:04:56 +0530

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Logo.css'
+import burgerLogo from '../../assets/Images/burger-logo.png';
+const logo = (props) =>{
+    return (
+         <div className = {classes.Logo} style={{height: props.height}}>
+           <img src={burgerLogo} alt = "My Burger"/>
+         </div>
+    );
+}
+
+export default logo;
